a5746635b33ddb02137b1dd76c53b352a502db37
phot0x <phot0x@Samis-MBP-Touch-2.local>
2020-06-09 14:27:02 -0400

    Cleaning up main server.js file and removing https blocks and fs module imports.

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,14 @@
-// EXPRESS/SERVER
-const fs = require('fs');
+// Express server imports.
 const config = require("./config");
 const express = require("express");
 const app = express();
 const http = require("http");
-const https = require("https");
 const httpServer = http.createServer(app);
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
-//Default security options
+// Default security options
 app.use(helmet());
-//MIDDLEWARE for adding the body property to req object
+// Middleware for adding the body property to req object.
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -18,56 +16,45 @@ app.use(
 );
 app.use(bodyParser.json());
 
-// MONGOOSE/DATABASE CONFIG
+// Mongoose and database configuration.
 const mongoose = require("mongoose");
-//Make Mongoose use `findOneAndUpdate()`. warning deprecation notice removal
+// Make Mongoose use `findOneAndUpdate()`. warning deprecation notice removal.
 mongoose.set("useFindAndModify", false);
-// MODELS
+// Database models import.
 const db = require("./models/index");
 // Make connection to Database
 db.connect();
 
-//CONTROLLERS
-// Adding user to internal db and mongodb
+// Register user to internal db controller.
 const addUser = require("./actions/addUser");
-// Checks and compares the hashed passwords plus sends accesstoken back
+// Authentication/login controller.
 const loginUser = require("./actions/loginUser");
-// Checks and compares the hashed passwords plus sends accesstoken back
+// Opt-in for beta-sign up controller.
 const optInUser = require("./actions/optInUser");
-// Public get restaurant controller
+
+// Public get restaurant controller.
 const getRestaurants = require("./routes/public/getRestaurants");
-// Public restaurant details
+// Public and private restaurant details controller.
 const restaurantDetails = require("./routes/authenticated/restaurantDetails");
-// Authenrticated routes which require access token.
+
+// Authenticated routes which require access token.
 const apiRouter = require("./routes/api");
 
-//AUTHENTICATED ROUTES
+// Authenticated routes -> located in routes/authenticated.
 app.use("/api", apiRouter);
 
-//PUBLIC ROUTES
+// Register user path.
 app.post("/register-user", addUser);
-// Login user route
+// Login user path.
 app.post("/login", loginUser);
-// Optin user for beta release.
+// Optin user for beta release path.
 app.post("/optin", optInUser);
-// Public routes for getting nearby restaurants.
+// Guest user - Public routes for getting nearby restaurants.
 app.get("/restaurants/:long/:lat", getRestaurants);
-// Public route for getting information about certain restaurant
+// Guest user - Public route for getting information about certain restaurant
 app.get("/restaurant/:id", restaurantDetails);
 
 // Running http version of server
 httpServer.listen(config.port, () => {
   console.log(`Example app listening on port ${config.port}!`);
 });
-
-// Cert locations
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/jyze.net/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/jyze.net/fullchain.pem')
-};
-
-// Https version of server
-https.createServer(options, app).listen(8001, () => {
-  console.log(`Example app listening on port 8001!`);
-});
-
